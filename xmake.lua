6130607597772853bb571528ad44e9143811ac9e
StarBeat <977663818@qq.com>
2022-10-22 21:03:51 +0800

    add xmake support

---
+++ xmake.lua
@@ -0,0 +1,12 @@
+add_rules("mode.release")
+target("box2d")
+    set_kind("shared")
+    set_runtimes("MD")
+    add_cflags("-g", "-DNDEBUG")
+    if is_plat("linux", "macosx") then
+        add_links("pthread", "m", "dl")
+    end
+    add_defines("LIQUIDFUN_EXTERNAL_LANGUAGE_API=1")
+    add_includedirs("liquidfun/Box2D/")
+    add_files("liquidfun/Box2D/Box2D/**.cpp")
+
